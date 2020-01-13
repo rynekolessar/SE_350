a4242888f78aac8d711ad68d9d763265658592e4
rynekolessar <ryne.kolessar@gmail.com>
2020-01-12 18:00:22 -0600

    2020-01-12

--- source/hw1/VideoObj.java
+++ source/hw1/VideoObj.java
@@ -1,100 +1,137 @@
 package hw1;
-// TODO:  complete the methods
+
+// TODO: complete the methods
 /**
- * Immutable Data Class for video objects.
- * Comprises a triple: title, year, director.
+ * Immutable Data Class for video objects. Comprises a triple: title, year,
+ * director.
  *
  * @objecttype Immutable Data Class
- * @objectinvariant
- *   Title is non-null, no leading or final spaces, not empty string.
- * @objectinvariant
- *   Year is greater than 1800, less than 5000.
- * @objectinvariant
- *   Director is non-null, no leading or final spaces, not empty string.
+ * @objectinvariant Title is non-null, no leading or final spaces, not empty
+ *                  string.
+ * @objectinvariant Year is greater than 1800, less than 5000.
+ * @objectinvariant Director is non-null, no leading or final spaces, not empty
+ *                  string.
  */
 final class VideoObj implements Comparable {
-  /** @invariant non-null, no leading or final spaces, not empty string */
-  private final String _title;
-  /** @invariant greater than 1800, less than 5000 */
-  private final int    _year;
-  /** @invariant non-null, no leading or final spaces, not empty string */
-  private final String _director;
+	/** @invariant non-null, no leading or final spaces, not empty string */
+	private final String _title;
+	/** @invariant greater than 1800, less than 5000 */
+	private final int _year;
+	/** @invariant non-null, no leading or final spaces, not empty string */
+	private final String _director;
 
-  /**
-   * Initialize all object attributes.
-   * Title and director are "trimmed" to remove leading and final space.
-   * @throws IllegalArgumentException if any object invariant is violated.
-   */
-  VideoObj(String title, int year, String director) {
-    // TODO  
-    _title = null;
-    _year = 0;
-    _director = null;
-     
-  }
+	/**
+	 * Initialize all object attributes. Title and director are "trimmed" to remove
+	 * leading and final space.
+	 * 
+	 * @throws IllegalArgumentException if any object invariant is violated.
+	 */
+	VideoObj(String title, int year, String director) {
+		if (title == null) {
+			throw new IllegalArgumentException();
+		}
 
-  /**
-   * Return the value of the attribute.
-   */
-  public String director() {
-    // TODO  
-    return "director";
-  }
+		if (director == null) {
+			throw new IllegalArgumentException();
+		}
 
-  /**
-   * Return the value of the attribute.
-   */
-  public String title() {
-    // TODO  
-    return "title";
-  }
+		title = title.trim();
+		director = director.trim();
 
-  /**
-   * Return the value of the attribute.
-   */
-  public int year() {
-    // TODO  
-    return -1;
-  }
+		if (title.length() == 0 || title.startsWith(" ") || title.endsWith(" ")) {
+			throw new IllegalArgumentException();
+		}
 
-  /**
-   * Compare the attributes of this object with those of thatObject.
-   * @param thatObject the Object to be compared.
-   * @return deep equality test between this and thatObject.
-   */
-  public boolean equals(Object thatObject) {
-    // TODO  
-    return false;
-  }
+		if (director.length() == 0 || director.startsWith(" ") || director.endsWith(" ")) {
+			throw new IllegalArgumentException();
+		}
 
-  /**
-   * Return a hash code value for this object using the algorithm from Bloch:
-   * fields are added in the following order: title, year, director.
-   */
-  public int hashCode() {
-    // TODO  
-    return -1;
-  }
+		if (year <= 1800 || year >= 5000) {
+			throw new IllegalArgumentException();
+		}
 
-  /**
-   * Compares the attributes of this object with those of thatObject, in
-   * the following order: title, year, director.
-   * @param thatObject the Object to be compared.
-   * @return a negative integer, zero, or a positive integer as this
-   *  object is less than, equal to, or greater thatObject.
-   * @throws ClassCastException if thatObject has an incompatible type.
-   */
-  public int compareTo(Object thatObject) {
-    // TODO  
-    return -1;
-  }
+		_title = title;
+		_year = year;
+		_director = director;
+	}
 
-  /**
-   * Return a string representation of the object in the following format:
-   * <code>"title (year) : director"</code>.
-   */
-  public String toString() {
-    // TODO  
-    return "El Mariachi (1996) : Rodriguez";
-  }
+	/**
+	 * Return the value of the attribute.
+	 */
+	public String director() {
+		return _director;
+	}
+
+	/**
+	 * Return the value of the attribute.
+	 */
+	public String title() {
+		return _title;
+	}
+
+	/**
+	 * Return the value of the attribute.
+	 */
+	public int year() {
+		return _year;
+	}
+
+	/**
+	 * Compare the attributes of this object with those of thatObject.
+	 * 
+	 * @param thatObject the Object to be compared.
+	 * @return deep equality test between this and thatObject.
+	 */
+	public boolean equals(Object thatObject) {
+		if (thatObject instanceof VideoObj) {
+			VideoObj other = (VideoObj) thatObject;
+			return (this._director.equals(other.director()) && this._title.equals(other.title())
+					&& this._year == other.year());
+		}
+		return false;
+	}
+
+	/**
+	 * Return a hash code value for this object using the algorithm from Bloch:
+	 * fields are added in the following order: title, year, director.
+	 */
+	public int hashCode() {
+		int result = 17;
+		int prime = 37;
+		result = prime * result + _title.hashCode();
+		result = prime * result + _year;
+		result = prime * result + _director.hashCode();
+		return result;
+	}
+
+	/**
+	 * Compares the attributes of this object with those of thatObject, in the
+	 * following order: title, year, director.
+	 * 
+	 * @param thatObject the Object to be compared.
+	 * @return a negative integer, zero, or a positive integer as this object is
+	 *         less than, equal to, or greater thatObject.
+	 * @throws ClassCastException if thatObject has an incompatible type.
+	 */
+	public int compareTo(Object thatObject) {
+		VideoObj other = (VideoObj) thatObject;
+
+		if (this._title.compareTo(other._title) != 0) {
+			return this._title.compareTo(other._title);
+		} else {
+			if (this._year != other._year) {
+				return this._year - other._year;
+			} else {
+				return this._director.compareTo(other._director);
+			}
+		}
+	}
+
+	/**
+	 * Return a string representation of the object in the following format:
+	 * <code>"title (year) : director"</code>.
+	 */
+	public String toString() {
+		return _title + " (" + _year + ") : " + _director;
+	}
 }
--- source/hw1/VideoTEST.java
+++ source/hw1/VideoTEST.java
@@ -1,83 +1,95 @@
 package hw1;
+
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 // TODO:  complete the tests
 public class VideoTEST extends TestCase {
-  public VideoTEST(String name) {
-    super(name);
-  }
-  public void testConstructorAndAttributes() {
-    String title1 = "XX";
-    String director1 = "XY";
-    String title2 = " XX ";
-    String director2 = " XY ";
-    int year = 2002;
+	@test
+	public VideoTEST(String name) {
+		super(name);
+	}
 
-    VideoObj v1 = new VideoObj(title1, year, director1);
-    Assert.assertSame(title1, v1.title());
-    Assert.assertEquals(year, v1.year());
-    Assert.assertSame(director1, v1.director());
+	@test
+	public void testConstructorAndAttributes() {
+		String title1 = "XX";
+		String director1 = "XY";
+		String title2 = " XX ";
+		String director2 = " XY ";
+		int year = 2002;
 
-    VideoObj v2 = new VideoObj(title2, year, director2);
-    Assert.assertEquals(title1, v2.title());
-    Assert.assertEquals(director1, v2.director());
-  }
+		VideoObj v1 = new VideoObj(title1, year, director1);
+		Assert.assertSame(title1, v1.title());
+		Assert.assertEquals(year, v1.year());
+		Assert.assertSame(director1, v1.director());
 
-  public void testConstructorExceptionYear() {
-    try {
-      new VideoObj("X", 1800, "Y");
-      Assert.fail();
-    } catch (IllegalArgumentException e) { }
-    try {
-      new VideoObj("X", 5000, "Y");
-      Assert.fail();
-    } catch (IllegalArgumentException e) { }
-    try {
-      new VideoObj("X", 1801, "Y");
-      new VideoObj("X", 4999, "Y");
-    } catch (IllegalArgumentException e) {
-      Assert.fail();
-    }
-  }
+		VideoObj v2 = new VideoObj(title2, year, director2);
+		Assert.assertEquals(title1, v2.title());
+		Assert.assertEquals(director1, v2.director());
+	}
 
-  public void testConstructorExceptionTitle() {
-    try {
-      new VideoObj(null, 2002, "Y");
-      Assert.fail();
-    } catch (IllegalArgumentException e) { }
-    try {
-      new VideoObj("", 2002, "Y");
-      Assert.fail();
-    } catch (IllegalArgumentException e) { }
-    try {
-      new VideoObj(" ", 2002, "Y");
-      Assert.fail();
-    } catch (IllegalArgumentException e) { }
-  }
+	@test
+	public void testConstructorExceptionYear() {
+		try {
+			new VideoObj("X", 1800, "Y");
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+		}
+		try {
+			new VideoObj("X", 5000, "Y");
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+		}
+		try {
+			new VideoObj("X", 1801, "Y");
+			new VideoObj("X", 4999, "Y");
+		} catch (IllegalArgumentException e) {
+			Assert.fail();
+		}
+	}
 
-  public void testConstructorExceptionDirector() {
-    // TODO  
-  }
+	@test
+	public void testConstructorExceptionTitle() {
+		try {
+			new VideoObj(null, 2002, "Y");
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+		}
+		try {
+			new VideoObj("", 2002, "Y");
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+		}
+		try {
+			new VideoObj(" ", 2002, "Y");
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+		}
+	}
 
-  public void testHashCode() {
-    Assert.assertEquals
-      (-875826552,
-       new VideoObj("None", 2009, "Zebra").hashCode());
-    Assert.assertEquals
-      (-1391078111,
-       new VideoObj("Blah", 1954, "Cante").hashCode());
-  }
+	@test
+	public void testConstructorExceptionDirector() {
+		// TODO
+	}
 
-  public void testEquals() { 
-    // TODO  
-  }
+	@test
+	public void testHashCode() {
+		Assert.assertEquals(-875826552, new VideoObj("None", 2009, "Zebra").hashCode());
+		Assert.assertEquals(-1391078111, new VideoObj("Blah", 1954, "Cante").hashCode());
+	}
 
-  public void testCompareTo() { 
-    // TODO  
-  }
+	@test
+	public void testEquals() {
+		// TODO
+	}
 
-  public void testToString() { 
-    // TODO  
-  }
+	@test
+	public void testCompareTo() {
+		// TODO
+	}
+
+	@test
+	public void testToString() {
+		// TODO
+	}
 }
