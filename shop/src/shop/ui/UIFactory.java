25a8a6ee1af51a6499b36d56957b53086bd7e1d7
rynekolessar <ryne.kolessar@gmail.com>
2020-02-23 22:33:35 -0600

    2020-02-23

--- shop/src/shop/ui/UIFactory.java
+++ shop/src/shop/ui/UIFactory.java
@@ -2,8 +2,8 @@ package shop.ui;
 
 public class UIFactory {
     private UIFactory() {}
-    static private UI _UI = new PopupUI();
-    //static private ui _UI = new TextUI();
+    //static private UI _UI = new PopupUI();
+    static private UI _UI = new TextUI();
     static public UI ui () {
         return _UI;
     }
